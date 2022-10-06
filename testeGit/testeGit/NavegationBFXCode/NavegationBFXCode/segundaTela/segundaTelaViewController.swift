389e96e9601e443aaedad9fd5263fbd082a1b105
Lorena1803 <lorena01ambrosio@gmail.com>
2022-10-06 19:04:18 -0300

    criação da primeira e segunda tela

---
+++ testeGit/testeGit/NavegationBFXCode/NavegationBFXCode/segundaTela/segundaTelaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  segundaTelaViewController.swift
+//  NavegationBFXCode
+//
+//  Created by Lorena on 06/10/22.
+//
+
+import UIKit
+
+class segundaTelaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
