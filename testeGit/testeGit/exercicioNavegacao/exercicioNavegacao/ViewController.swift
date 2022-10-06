f2d727fcb1b9945d857c1a0b3e693ad9bb3cfe05
Lorena1803 <lorena01ambrosio@gmail.com>
2022-10-06 20:11:22 -0300

    mudando a ação do botão pra código

--- testeGit/testeGit/exercicioNavegacao/exercicioNavegacao/ViewController.swift
+++ testeGit/testeGit/exercicioNavegacao/exercicioNavegacao/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonTela1: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionButtonTela1(_ sender: UIButton) {
+        performSegue(withIdentifier: "tela2", sender: nil)
+    }
+    
 }
 
