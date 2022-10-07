3aaf05df251d060ab5474ec63e4eaa0f7c8e2906
Lorena1803 <lorena01ambrosio@gmail.com>
2022-10-06 23:00:57 -0300

    criei navegacao de telas por codigo usando present e push

--- testeGit/testeGit/exercicioNavegacao/exercicioNavegacao/ViewController.swift
+++ testeGit/testeGit/exercicioNavegacao/exercicioNavegacao/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
-    @IBAction func tappedTela2(_ sender: UIButton) {
-        performSegue(withIdentifier: "tela2", sender: nil)
+    @IBAction func tappedModal(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "tela02ViewController", bundle: nil).instantiateViewController(withIdentifier: "tela02ViewController") as? tela02ViewController
+        present(vc ?? UIViewController(), animated: true)
+        
     }
     
+    
+    @IBAction func tappedPush(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "tela02ViewController", bundle: nil).instantiateViewController(withIdentifier: "tela02ViewController") as? tela02ViewController
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
     
     
     }
