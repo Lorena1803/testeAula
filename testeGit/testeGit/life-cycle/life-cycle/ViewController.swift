b7ee7295e6eccd8e665bf68990120cfda1deed21
Lorena1803 <lorena01ambrosio@gmail.com>
2022-10-11 20:56:30 -0300

    criei um projeto para ver como funciona o life cycle

---
+++ testeGit/testeGit/life-cycle/life-cycle/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  life-cycle
+//
+//  Created by Lorena on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+}
+
