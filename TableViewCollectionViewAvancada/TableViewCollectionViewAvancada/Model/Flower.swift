aaad3e643cba27505656d946f3c096c98aa92f28
Lorena1803 <lorena01ambrosio@gmail.com>
2022-11-10 19:57:04 -0300

    dando continuidade ao projeto tableview com collectionview avancada

---
+++ TableViewCollectionViewAvancada/TableViewCollectionViewAvancada/Model/Flower.swift
@@ -0,0 +1,13 @@
+//
+//  Flower.swift
+//  TableViewCollectionViewAvancada
+//
+//  Created by Lorena on 09/11/22.
+//
+
+import Foundation
+
+struct Flower{
+    var name: String
+    var listFlowers: [String]
+}
